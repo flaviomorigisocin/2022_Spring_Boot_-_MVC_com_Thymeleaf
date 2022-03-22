c75d7f78c8b8f030c39a58e27b5d98cdbb5e4649
Morigi <61834680+flaviomorigisocin@users.noreply.github.com>
2022-03-22 20:48:34 -0300

    Add files via upload

---
+++ src/main/java/com/mballem/curso/boot/web/validator/FuncionarioValidator.java
@@ -0,0 +1,33 @@
+package com.mballem.curso.boot.web.validator;
+
+import java.time.LocalDate;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.mballem.curso.boot.domain.Funcionario;
+
+public class FuncionarioValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		System.out.println("supports: " + Funcionario.class.equals(clazz));
+		return Funcionario.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object object, Errors errors) {
+		System.out.println("validate: true");
+		
+		Funcionario f = (Funcionario) object;
+		
+		LocalDate entrada = f.getDataEntrada();
+		
+		if (f.getDataEntrada() != null && f.getDataSaida() != null) {
+			if (f.getDataSaida().isBefore(entrada)) {
+				errors.rejectValue("dataSaida", "PosteriorDataEntrada.funcionario.dataSaida");
+			}
+		}
+	}
+
+}
